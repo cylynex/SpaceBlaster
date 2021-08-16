29bc82a5bfd27c5bc2fbbb2eb4a52108c5a02345
Santo Commarato <cylynex@gmail.com>
2021-08-16 11:09:40 -0600

    Upgraded movement, boundaries

--- Space Blaster/Assets/Scripts/Player.cs
+++ Space Blaster/Assets/Scripts/Player.cs
@@ -9,21 +9,33 @@ public class Player : MonoBehaviour {
 
     [SerializeField] float moveSpeed = 10f;
 
+    float xMin, xMax;
+    float yMin, yMax;
+
     private void Start() {
-        
+        SetupBounds();
     }
 
     private void Update() {
         Move();
     }
 
+    
+    void SetupBounds() {
+        Camera gameCamera = Camera.main;
+        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0.1f, 0, 0)).x;
+        xMax = gameCamera.ViewportToWorldPoint(new Vector3(0.9f, 0, 0)).x;
+        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0.05f, 0)).y;
+        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 0.5f, 0)).y;
+    }
+
 
     private void Move() {
         float deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
-        float newXPos = transform.position.x + deltaX;
-
         float deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
-        float newYPos = transform.position.y + deltaY;
+
+        float newXPos = Mathf.Clamp((transform.position.x + deltaX), xMin, xMax);
+        float newYPos = Mathf.Clamp((transform.position.y + deltaY), yMin, yMax);
 
         transform.position = new Vector2(newXPos, newYPos);
 
