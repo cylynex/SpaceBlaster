a7cf2049a8a5324532a47a1d665eec8921f9f29f
Santo Commarato <cylynex@gmail.com>
2021-08-16 18:13:14 -0600

    Started on Spawner

---
+++ Space Blaster/Assets/Scripts/Spawner.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+    [SerializeField] List<Wave> waves = new List<Wave>();
+    int currentWave = 0;
+
+    void Start() {
+        print("spawner online");
+
+
+
+    }
+
+}
