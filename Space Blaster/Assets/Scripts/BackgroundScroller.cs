6df3255051f84a5894c63fa0c34b22b50dbcbfd9
Santo Commarato <cylynex@gmail.com>
2021-08-23 09:31:33 -0600

    Moving background

---
+++ Space Blaster/Assets/Scripts/BackgroundScroller.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScroller : MonoBehaviour {
+
+    [SerializeField] float backgroundScrollSpeed = 0.5f;
+    Material bgMaterial;
+    Vector2 offset;
+
+    private void Start() {
+        bgMaterial = GetComponent<Renderer>().material;
+        offset = new Vector2(0, backgroundScrollSpeed);
+    }
+
+    private void Update() {
+        bgMaterial.mainTextureOffset += offset * Time.deltaTime;
+    }
+
+}
